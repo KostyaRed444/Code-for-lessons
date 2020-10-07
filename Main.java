34b95d270c4892a18d5a44d1e05c243ddc284ca3
KostyaRed444 <71414432+KostyaRed444@users.noreply.github.com>
2020-10-07 10:20:03 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package geekbrains.lesson_1.step_6;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("C:/Users/1052126/IdeaProjects/untitled5/src/geekbrains/lesson_1/step_6/Sales.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
