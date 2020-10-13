ce8720272ff39475e66a48b30fc3f8a1d9b95202
KostyaRed444 <71414432+KostyaRed444@users.noreply.github.com>
2020-10-13 09:17:33 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,49 @@
-package geekbrains.lesson_1.step_6;
+import java.util.Scanner;
+import java.util.TreeMap;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Input your date:");
 
-public class Main {
+        Scanner scan=new Scanner(System.in);
+        String name; //name of customer
+        String product; //name of product
+        int count; //count of product
 
-    public static void main(String[] args) {
-        try {
-            File file = new File("C:/Users/1052126/IdeaProjects/untitled5/src/geekbrains/lesson_1/step_6/Sales.txt");
-            FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);
-            String line = reader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = reader.readLine();
+        TreeMap<String, TreeMap<String,Integer>> customers= new TreeMap<>();//create TreeMap (for products) in TreeMap(for customers)
+
+        String[] str= scan.nextLine().split(" "); //cut our string
+
+        while (!str[0].isEmpty()){
+            TreeMap<String, Integer> products = new TreeMap<>(); //new TreeMap for product
+
+            name= str[0];
+            product= str[1];
+            count=Integer.parseInt(str[2]);
+
+            if(customers.containsKey(name)) { //create twig for customer on name
+                products = customers.get(name);
+                if (products.containsKey(product)) { //twig for product on name of product
+                    count += products.get(product); //get count of product
+                    products.put(product, count);
+                } else {
+                    products.put(product, count);
+                }
+                customers.put(name, products);
+            }else {
+                products.put(product, count);
+                customers.put(name, products);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+                str=scan.nextLine().split(" ");
+            }
+
+            for(String customer: customers.keySet()){ //output our TreeMap
+                System.out.println(customer+":"); //name of customer
+                for(String prod: customers.get(customer).keySet()){ // products and they count
+                    System.out.println(prod+" "+customers.get(customer).get(prod));
+                }
+                System.out.print('\n');
+            }
         }
+
     }
-}
