1c880440cd9834f644b75c706f0a188e87825089
KostyaRed444 <71414432+KostyaRed444@users.noreply.github.com>
2020-11-15 15:27:06 +0800

    Update MainActivity.java

--- Android/Marking/MainActivity.java
+++ Android/Marking/MainActivity.java
@@ -7,28 +7,25 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    String[] citiesOut = {"Иркутск", "Ангарск"};
+    String[] citiesOut;
 
-    String[] citiesIn = {"Москва", "Киото", "Шанхай", "Рио-де-Жанейро", "Нью-Йорк", "Лондон"};
+    String[] citiesIn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        citiesOut= getResources().getStringArray(R.array.citiesOut);
+        citiesIn= getResources().getStringArray(R.array.citiesIn);
+
         Spinner spinnerOut = (Spinner) findViewById(R.id.citiesOut);
-        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
         ArrayAdapter<String> adapterOut = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, citiesOut);
-        // Определяем разметку для использования при выборе элемента
         adapterOut.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Применяем адаптер к элементу spinner
         spinnerOut.setAdapter(adapterOut);
 
         Spinner spinnerIn = (Spinner) findViewById(R.id.citiesIn);
-        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
         ArrayAdapter<String> adapterIn = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, citiesIn);
-        // Определяем разметку для использования при выборе элемента
         adapterIn.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Применяем адаптер к элементу spinner
         spinnerIn.setAdapter(adapterIn);
     }
 }
