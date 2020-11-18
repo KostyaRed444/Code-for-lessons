37dd02741f5842f992b36a90f9248697293b479c
KostyaRed444 <71414432+KostyaRed444@users.noreply.github.com>
2020-11-18 13:36:49 +0800

    Update MainActivity.java

--- Android/Guess/MainActivity.java
+++ Android/Guess/MainActivity.java
@@ -1,28 +1,31 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText minInputLayout;
-    EditText  maxInputLayout;
+    EditText minText, maxText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        minInputLayout=(EditText) findViewById(R.id.min_num);
-        maxInputLayout=(EditText) findViewById(R.id.max_num);
+        minText = (EditText) findViewById(R.id.min);
+        maxText = (EditText) findViewById(R.id.max);
+
     }
 
-    public void next_act(View view) {
-        Intent intent = new Intent(MainActivity.this, guess_activity.class);
-        int min=Integer.parseInt(minInputLayout.getText().toString());
-        int max=Integer.parseInt(maxInputLayout.getText().toString());
-        intent.putExtra("min", min);
-        intent.putExtra("max", max);
-        startActivity(intent);
+    public void onStartGameClick(View v)
+    {
+        String min = minText.getText().toString();
+        String max = maxText.getText().toString();
+
+        Intent i = new Intent(this, GameActivity.class);
+        i.putExtra("max", max);
+        i.putExtra("min", min);
+        startActivity(i);
     }
 }
