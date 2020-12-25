a5007dfd1d815d1f6960624e090343d6bb900bac
KostyaRed444 <71414432+KostyaRed444@users.noreply.github.com>
2020-12-25 15:02:28 +0800

    Update MainActivity.java

--- Android/Final/MainActivity.java
+++ Android/Final/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.list);
 
         listLang=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, languages);
         listLang.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
